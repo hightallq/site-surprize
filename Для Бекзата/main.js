5c0c08eb3f2e27d2daae41ed7df20a7679c244c5
Bruno <77980627+hightallq@users.noreply.github.com>
2022-08-08 22:24:18 +0300

    Delete Для Бекзата directory

--- Для Бекзата/main.js
+++
@@ -1,8 +0,0 @@
-let menu =document.getElementById("icon");
-
-let aside =document.querySelector("aside");
-
-menu.onclick = function () {
-    this.classList.toggle("active");
-    aside.classList.toggle("active");
-}
